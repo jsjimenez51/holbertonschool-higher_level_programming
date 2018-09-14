ef6e8d94718887b9416bd77044c91ee57eef00a8
Joshua Jimenez <jsjimenez51@gmail.com>
2018-09-14 16:54:03 +0000

    Create file 2-arg.py

---
+++ 0x02-python-import_modules/2-arg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    count = len(argv)
+    if count == 1:
+        print("0 arguments.")
+    elif count == 2:
+        print("1 argument:")
+        print("1: {:s}".format(argv[1]))
+    else:
+        print("{:d} arguments:".format(count -1))
+        num = 1
+        for i in range(1, count):
+            print("{:d}: {:s}".format(num, argv[i]))
+            num = num + 1
