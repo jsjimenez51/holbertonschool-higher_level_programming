bc9e8da82095e2e93928b933f854612a7f8f9334
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-02 21:50:00 +0000

    Created file 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Defines a Square Class
+"""
+
+
+class Square:
+    """
+    A Class called Square with an attribute:
+    size: the size of the Square.
+    """
+    def __init__(self, size=0):
+        """
+        Instantiated with size.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        A public method that returns the area of the Square.
+        """
+        return self.__size ** 2
