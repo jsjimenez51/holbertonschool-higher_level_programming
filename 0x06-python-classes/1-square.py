f383434bc847e3e612b94080c6fe66d55ffce8ca
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-02 21:29:42 +0000

    Created files 1-square.py and 2-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a Square Class
+"""
+
+
+class Square:
+    """
+    A Class called Square with an attribute:
+    size: the size of the Square.
+    """
+    def __init__(self, size=0):
+        """
+        Instantiated with size
+        """
+        self.__size = size
