4151bcd8ea643b2f0894ace8623b02d16e3bfdb9
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-12 20:12:45 +0000

    Refactored test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,39 +6,66 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """ Tests for 6-max_integer.py"""
+    def test_docstring(self):
+        self.assertTrue(len(__import__('6-max_integer').__doc__) > 1)
 
-    def testForSelf(self):
-        pass
+    def test_func_doc(self):
+        self.assertTrue(len(max_integer.__doc__) > 1)
 
-    def test_10_is_max(self):
-        """is 10 the max integer?"""
-        self.assertEqual(max_integer([2, 4, 6, 10]), 10)
+    def test_emptyargs(self):
+        self.assertIsNone(max_integer())
 
-    def test_max_in_middle(self):
-        """max is placed in middle"""
-        self.assertEqual(max_integer([5, 15, 25, 8]), 15)
+    def test_empty(self):
+        self.assertIsNone(max_integer([]), None)
+        self.assertIsNone(max_integer([None]), None)
+        with self.assertRaises(TypeError):
+            max_integer(None)
+        with self.assertRaises(TypeError):
+            max_integer([2, 4, 5, 6, None])
 
-    def test_len_0(self):
-        """is the length of matrix 0"""
-        self.assertEqual(max_integer([]), None)
+    def test_one(self):
+        self.assertEqual(max_integer([55]), 55)
 
-    def test_negative(self):
-        """how it reacts to negative numbers"""
-        self.assertEqual(max_integer([3, -2, -1, -100]), 3)
+    def test_integers(self):
+        self.assertEqual(max_integer([1, 5, 6, 4]), 6)
+        self.assertEqual(max_integer([55, 55, 55, 55]), 55)
+        self.assertEqual(max_integer([6, 2, 3, 4]), 6)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([3]), 3)
 
-    def test_not_matrix(self):
-        """no matrix given"""
-        self.assertEqual(max_integer([7]), 7)
+    def test_integer_neg(self):
+        self.assertEqual(max_integer([-44, -66, -43, -22]), -22)
+        self.assertEqual(max_integer([1, 3, 74, -2]), 74)
 
-    def test_double_matrix(self):
-        """ given double matrix"""
-        self.assertEqual(max_integer([[4, 8], [1, 7]]), [1, 7])
+    def test_float(self):
+        self.assertEqual(max_integer([2.2, 3.3, 4.4, 5.5]), 5.5)
+        self.assertEqual(max_integer([-44.5, -53.22, -2.224, -3.14]), -2.224)
 
-    def test_matrix_letters(self):
-        """ only letters in matrix """
-        self.assertEqual(max_integer(["A", "B", "C"]), 'B')
+    def test_string(self):
+        self.assertEqual(max_integer("12345323121"), "5")
+        self.assertEqual(max_integer("1, 2, 3"), "3")
 
+    def test_alpha(self):
+        self.assertEqual(max_integer(["xyz"]), "xyz")
+        self.assertEqual(max_integer(["abc", "xyz"]), "xyz")
+        self.assertEqual(max_integer(["abc", "1", "4", "xyz"]), "xyz")
 
-if __name__ == '__main__':
+    def test_other_type(self):
+        self.assertEqual(max_integer([[1, 2, 3, 4], [1, 4, 3, 2]]), [1, 4, 3, 2])
+        self.assertEqual(max_integer((3, 4, 5, 6, 19)), 19)
+        with self.assertRaises(KeyError):
+            max_integer({1: 1, 4: 5, 5: 4})
+
+    def test_mix(self):
+        with self.assertRaises(TypeError):
+            max_integer(["help", 22, "44", 4.5, -3])
+        with self.assertRaises(TypeError):
+            max_integer(["help", [22], "44", 5.5, -55])
+
+    def test_none(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+if __name__ == "__main__":
     unittest.main()
