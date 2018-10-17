19a806bbdc9c04aa0000ee91b725afd0aede7f56
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-17 03:19:16 +0000

    Removed additional space in encoding call

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
     Creates an object from a JSON file
     """
-    with open(filename, 'r', encoding ='utf-8') as a_file:
+    with open(filename, 'r', encoding='utf-8') as a_file:
         return json.load(a_file)
