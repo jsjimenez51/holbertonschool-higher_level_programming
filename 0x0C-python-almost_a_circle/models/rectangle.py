aa4dd8ccd9737f18de2e3d16cddd368e8de1aaf8
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-18 19:19:53 +0000

    added in area, display, and __str__ attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,91 +25,108 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            """
-            Returns the width of the Rectangle
-            """
-            return (self.__width)
+    @property
+    def width(self):
+        """
+        Returns the width of the Rectangle
+        """
+        return (self.__width)
 
-        @width.setter
-        def width(self, value):
-            """
-            Sets the value of width
+    @width.setter
+    def width(self, value):
+        """
+        Sets the value of width
 
-            Attributes:
-                value (int): value of the width
-            """
-            if not isinstance(value, int):
-                raise TypeError('width must be an integer')
-            if value <= 0:
-                raise ValueError('width must be > 0')
-            self.__width = value
+        Attributes:
+        value (int): value of the width
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
 
-        @property
-        def height(self):
-            """
-            Returns the height of the Rectangle
-            """
-            return self.__height
+    @property
+    def height(self):
+        """
+        Returns the height of the Rectangle
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
-            Set the value of height
+    @height.setter
+    def height(self, value):
+        """
+        Set the value of height
 
-            Attributes:
-                value (int): value of the height
-            """
-            if not isinstance(value, int):
-                raise TypeError('height must be an integer')
-            if value <= 0:
-                raise ValueError('height must be > 0')
-            self.__height = value
+        Attributes:
+        value (int): value of the height
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
 
-        @property
-        def x(self):
-            """
-            Returns x
-            """
-            return self.__x
+    @property
+    def x(self):
+        """
+        Returns x
+        """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """
-            Sets the value of x
+    @x.setter
+    def x(self, value):
+        """
+        Sets the value of x
 
-            Attribute:
-                value (int): value to set x to
-            """
-            if not isinstance(value, int):
-                raise TypeError('x must be an integer')
-            if value < 0:
-                raise TypeError('x must be >= 0')
-            self.__x = value
+        Attribute:
+        value (int): value to set x to
+        """
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
 
-        @property
-        def y(self):
-            """
-            Returns y
-            """
-            return self.__y
+    @property
+    def y(self):
+        """
+        Returns y
+        """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """
-            Sets the value of y
+    @y.setter
+    def y(self, value):
+        """
+        Sets the value of y
 
-            Attributes:
-                value (int): valut to set y to
-            """
-            if not isinstance(value, int):
-                raise TypeError('y must be an integer')
-            if value < 0:
-                raise ValueError('y must be >= 0')
+        Attributes:
+        value (int): valut to set y to
+        """
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
 
-        def area(self):
-            """
-            Returns the area of the rectangle
-            """
-            return self.__width * self.__height
+    def area(self):
+        """
+        Returns the area of the rectangle
+        """
+        return (self.width * self.height)
+
+    def display(self):
+        """
+        Prints the Rectangle with #s to stdout
+        """
+        for row in range(self.y):
+            print()
+        for row in range(self.height):
+            print('{}{}'.format(' ' * self.x, '#' * self.width))
+
+    def __str__(self):
+        """
+        Overrides the Rectanlge class __str__ method
+        """
+        return ('[Rectangle] ({}) {}/{} - {}/{}'
+                .format(self.id, self.x, self.y, self.width, self.height))
