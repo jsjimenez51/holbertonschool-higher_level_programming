d82e959c8e7e58ec90102d63c0f33eab10b6f4a1
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-18 20:49:03 +0000

    Added in Update attribute to square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,9 +21,42 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        Returns the size of the square
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of size
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Returns a string representation of square (overload of Rectangle attr)
         """
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                   self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns new arguments to each attribute
+        """
+        update = len(args)
+        if update is not 0:
+            if update >= 1:
+                self.id = args[0]
+            if update >= 2:
+                self.size = args[1]
+            if update >= 3:
+                self.x = args[2]
+            if update >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
