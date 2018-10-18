373db5a8981665299f057267f9671e649385b090
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-18 21:16:20 +0000

    Added to_dictionary method to square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -60,3 +60,12 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns a dictionary representation of the Square
+        """
+        return {'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y}
