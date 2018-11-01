fdb1f4cf3ec44a9a0f686d66de74571958a0d40a
Joshua Jimenez <jsjimenez51@gmail.com>
2018-11-01 03:33:03 +0000

    UPDATE: refactored code for 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""
-Module that defines a class called MyList.
-"""
-
-
 class MyList(list):
-    """Class that inherits from list to build a sorted list.
     """
-
+    Class that inherits from list to build a sorted list.
+    """
     def print_sorted(self):
-        """Prints a sorted list.
+        """
+        Prints a sorted list.
         """
         print(sorted(self))
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testfile("./tests/1-my_list.txt")
