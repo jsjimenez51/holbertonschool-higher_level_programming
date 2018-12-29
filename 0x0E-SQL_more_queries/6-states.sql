2117291dd662599659ddd5acb23a122733897b07
Joshua Jimenez <jsjimenez51@gmail.com>
2018-12-29 19:58:43 +0000

    FIX: line 5 table name was corrected

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- states desc: id INT unique, auto gen, not null, and primary key
 -- name VC256 not null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
