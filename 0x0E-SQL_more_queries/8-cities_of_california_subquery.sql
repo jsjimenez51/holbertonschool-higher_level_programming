a92b1a2453c3af0f57cf09e8582ae54ee2cc8b1d
Joshua Jimenez <jsjimenez51@gmail.com>
2018-12-30 00:07:09 +0000

    UPDATE: refactored selection lines

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,8 @@
 -- lists all the related data in a table found in a MySQL database
 -- record to be found is California but ids can be different
 -- results must be sorted in ascending order
-SELECT 'id', 'name'
-FROM cities
+SELECT id, name  FROM cities
 WHERE state_id = (
 	SELECT id FROM states
-	WHERE name = 'California')
+	WHERE name = "California")
 ORDER BY cities.id ASC;
