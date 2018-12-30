8da4761718c25c379a3f4a18d6989741f7c3d35e
Joshua Jimenez <jsjimenez51@gmail.com>
2018-12-30 02:14:55 +0000

    FIX: line 4 join corrected

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- import a database dump to a MySQL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+LEFT outer JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
