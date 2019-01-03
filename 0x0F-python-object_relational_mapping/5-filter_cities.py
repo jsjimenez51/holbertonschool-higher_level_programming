ed14dff8eea6bd0282394e50996c373cf09540ce
Joshua Jimenez <jsjimenez51@gmail.com>
2019-01-03 18:54:16 +0000

    UPDATE: quick fix to import again

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,13 +3,13 @@
 
 if __name__ == '__main__':
     import MySQLdb
-    from sys import argv
+    import sys
 
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
                          charset="utf8")
     cur = db.cursor()
     state_cities = "Select cities.name \
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                     JOIN states ON states.id = cities.state_id \
                     WHERE states.name = %s \
                     ORDER BY cities.id ASC"
-    cur.execute(state_cities, (argv[4],))
+    cur.execute(state_cities, (sys.argv[4],))
     this_state = cur.fetchall()
     print(", ".join([these_cities[0] for these_cities in this_state]))
     cur.close()
