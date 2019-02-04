85b996a5859b425ac45f01e6cbea3e6b58af69c4
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-04 14:51:28 -0800

    ADD: file for task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches a website's info and displays it
+"""
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    with urlopen("https://intranet.hbtn.io/status") as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: ".format(content.decode('utf-8')))
