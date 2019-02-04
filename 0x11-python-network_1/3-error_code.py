d22e7c9b182920ae4750d84c771ca0a30fa3c26e
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-04 15:43:46 -0800

    ADD: file for task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response
+handles errors and prints out the HTTP status code
+argv[1]: URL
+"""
+from urllib.request import urlopen
+from urllib.parse import urlencode
+from urllib.error import URLError
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+        except HTTPError as e:
+            print("Error code: {}".format(e.code))
