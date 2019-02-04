95a52dd8798b022e2ff43d7482698c0b7704da85
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-03 20:54:46 -0800

    ADD: all files related to task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds peak using a binary search
+    """
+    if list_of_integers:
+        start = 0
+        end = len(list_of_integers) - 1
+        if start == end:
+            return list_of_integers[start]
+        # checks if the start or end of the list are peaks
+        if list_of_integers[start] > list_of_integers[1]:
+            return list_of_integers[start]
+        if list_of_integers[end] > list_of_integers[end - 1]:
+            return list_of_integers[end]
+        # finds the mid point of the list to begin binary search
+        mid = (end -start) // 2
+        if list_of_integers[mid] < list_of_integers[mid - 1]:
+            return find_peak(list_of_integers[:mid])
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            return find_peak(list_of_integers[mid + 1:])
+        return list_of_integers[mid]
+    return None
