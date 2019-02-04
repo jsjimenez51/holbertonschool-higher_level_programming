cd613bed62aac82e9213ffa1db3f5c35b05af3f4
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-04 15:03:39 -0800

    UPDATE: missing brackets for utf8 content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,8 @@ from urllib.request import urlopen
 if __name__ == "__main__":
     with urlopen("https://intranet.hbtn.io/status") as response:
         content = response.read()
+        lang = content.decode("utf-8")
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: ".format(content.decode('utf-8')))
+        print("\t- utf8 content: {}".format(content))
