011d68fde5aa90e86f549bfc207cf18cbc99bd18
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-04 16:55:50 -0800

    ADD: file for task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+sends a request to a URL and displays the body of the response
+"""
+from requests import get
+from request.exceptions import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = get(argv[1])
+    try:
+        req.raise_for_status()
+    except:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
