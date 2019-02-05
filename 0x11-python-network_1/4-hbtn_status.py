044fb09d1d824ec4568fe1cefa2f17f45bd0645e
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-04 16:25:28 -0800

    UPDATE: changed format for content print statement

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     req = get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.content.decode('utf-8')))
+    print("\t- content: {}".format(req.text))
