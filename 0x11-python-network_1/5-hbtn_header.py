b00247053b1237f5ca430ba8b6fbfabe1cec3506
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-04 16:31:46 -0800

    ADD: file for task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+sends a request to the URL and displays the value of X-Request-Id
+""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    req = get(argv[1])
+    print(req.headers.get('X-Request-Id'))
