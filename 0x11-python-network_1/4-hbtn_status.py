a0f1b4441e80fd7b56c9c9ac20008af93f760747
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-04 16:22:22 -0800

    ADD: file for task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+fetches content from the holberton intranet
+"""
+from requests import get
+
+
+if __name__ == "__main__":
+    req = get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.content.decode('utf-8')))
