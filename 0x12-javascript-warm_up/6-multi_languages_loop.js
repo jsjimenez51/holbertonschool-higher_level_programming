45e038986fa8ef77aed14695a114e6a10a6b8b8d
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-12 14:29:35 -0800

    ADD: file for task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints 3 lines by using an array of strings and a loop
+
+let strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+let idx = 0;
+for (idx in strings) console.log(strings[idx]);
