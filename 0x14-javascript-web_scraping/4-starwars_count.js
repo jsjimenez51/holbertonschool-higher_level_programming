7515d8d92c34c923ab66c2e549eb0522f95d02fa
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-19 13:02:13 -0800

    ADD: file for task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// prints the number of movies where the character "Wedge Antilles" is there
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let data = JSON.parse(body);
+    let movieData = (data.results);
+    let wedgeID = /18/;
+    for (let movie of movieData) {
+      let chars = movie.characters;
+      for (let isWedge of chars) {
+        if (wedgeID.test(isWedge) === true) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+})
